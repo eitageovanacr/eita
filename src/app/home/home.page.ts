eb866d5e395d6dabd2c768afaba96d1e4717f73b
eitageovanacr <geo.costa.r@gmail.com>
2019-03-18 12:14:15 -0300

    Versao2

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  valor1 : number = 0;
+    valor2 : number = 0;
+
+
+  somar(): void{
+    let total = Number(this.valor1) + Number(this.valor2);
+    alert("a soma é"+" "+total);
+
+    if(valor1==0){
+        alert("O numero "+this.valor+"é par")
+    }
+  }else{
+       alert("O numero"+this.valor+"é impar")
+  }
 
 }
